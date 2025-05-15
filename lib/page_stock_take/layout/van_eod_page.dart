b3c1d1508eebe33c180ed45f8630bc2687683ec9
daushahrum <firdaus.shahrum@amast.com.my>
2025-05-15 15:26:55 +0800

    Van EOD Stock Take fix 2

--- lib/page_stock_take/layout/van_eod_page.dart
+++ lib/page_stock_take/layout/van_eod_page.dart
@@ -773,7 +773,7 @@ class _VanEODPageState extends State<VanEODPage> {
                                                     SizedBox(
                                                       width: MediaQuery.of(context).size.width * 0.2,
                                                       child: TextFormField(
-                                                        initialValue: '${element['init_qty']}',
+                                                        initialValue: '0',
                                                         keyboardType: TextInputType.number,
                                                         textAlign: TextAlign.start,
                                                         style: TextStyle(color: biruImran2),
